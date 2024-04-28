6e82a87edafff81638d96e2d1b3b4397a2f398fe
Carlos García Pérez <carlosziok@gmail.com>
2024-04-28 20:49:18 +0300

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Carlos Garcia Perez on 28/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
